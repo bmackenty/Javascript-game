74ed40c7aa22bb36af1319ade9a893cc1475e3fe
Bill MacKenty <bmackenty@users.noreply.github.com>
2018-11-07 16:38:40 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+
+This is a javascript implementation of a simple ascii art RPG game
+
+*/
+var temperature = 37.5;
+var d20 = Math.floor(Math.random() * 20)+1;
+temperature = Math.round(temperature);
+document.write(d20);
+
+var counter = 1;
+while ( counter <= 10 ) {
+  console.log(counter);
+  counter += 1;
+}
