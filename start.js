136f4892192c2abe906deb360f12b15654d94866
Bill MacKenty <bmackenty@users.noreply.github.com>
2018-11-09 22:32:35 +0100

    using font awesome for terrain.
    kind of cool. issue with screen size but overall, it looks pretty good. need to make custom SVG for open ground.

--- start.js
+++ start.js
@@ -43,19 +43,20 @@ var grid = [];
 var terrain;
 // the variable below tracks the terrain type the player moved from. When they move again, we replace the old terrain stored in this variable.
 // the reason we have empty terrain is for the very first move of the game.
-var destination_terrain = '...';
+var destination_terrain = '<i class=\"fas fa-ellipsis-h fa-fw\"></i>';
+// the line below is because I was testing row length and got annoyed having to continually adjust 
 
 var counter = 1;
-while ( counter <= 695 ) {
+while ( counter <= 1189 ) {
   // the line below creates some random terrain. totally random, TODO: this needs to be smart.
   terrain = Math.floor(Math.random() * 5)+1;
   grid.push(terrain);
   counter += 1;
 }
 
-// make player starting location. It's 100 just TOTALLY for testing. Also, we should track the current player location for
+// make player starting location. It's 1 just TOTALLY for testing. Also, we should track the current player location for
 // reasons. 
-grid.splice(100, 0, 6);
+grid.splice(10, 0, 6);
 
 
 
@@ -65,24 +66,19 @@ var counter = 0;
 var arrayLength = grid.length;
 for (var i = 0; i < arrayLength; i++) {
 
-  counter = counter +1;
-  if(counter == 24) {
-    grid[i] = "\n";
-    counter = 0;
-  }
 
   if (grid[i] === 1 || grid[i] === 2 || grid[i] === 3){
     // this is plain, open terrain
-    grid[i] = "...";
+    grid[i] = "<i class=\"fas fa-ellipsis-h fa-fw\"></i>";
   } else if (grid[i] === 4) {
     // this is a tree
-    grid[i] = ".*.";
+    grid[i] = "<i class=\"fas fa-tree fa-fw\" style=\"color:green\"></i>";
   } else if (grid[i] === 5) {
     // this is a mountain or hill
-    grid[i] = ".^.";
+    grid[i] = "<i class=\"fas fa-mountain fa-fw\" style=\"color:grey\"></i>";
   } else if (grid[i] === 6) {
   // this is a mountain or hill
-  grid[i] = "[P]";
+  grid[i] = "<i class=\"fas fa-child fa-fw\" style=\"color:red\"></i>";
 }
 
 }
@@ -94,62 +90,66 @@ return grid;
 function move(direction) {
 
   if (direction === 'r'){
-    // let's start by getting ther current location of the player
-    var current_location = grid.indexOf('[P]');
+    // let's start by getting the current location of the player
+    var current_location = grid.indexOf('<i class=\"fas fa-child fa-fw\" style=\"color:red\"></i>');
     // the line below is for debugging
     console.log(current_location);
-    // now the destination. This ASSUMES A 24 LENGTH array
+    // now the destination. This ASSUMES A 34 LENGTH array
     var destination = current_location + 1;
     // now lets replace the terrain that was in the old place.
-    grid[destination-1] = destination_terrain;
+    grid[destination - 1] = destination_terrain;
     // now let's get the terrain the place they want to go. we need this so we can replace it when they move later on. 
     destination_terrain = grid[current_location + 1];
     // now let's move the player icon. 
-    grid[destination] = '[P]';
+    grid[destination] = '<i class=\"fas fa-child fa-fw\" style=\"color:red\"></i>';
+    document.getElementById("messages").innerHTML = current_location;
 
 
   } else if (direction === 'l'){
-        // let's start by getting ther current location of the player
-        var current_location = grid.indexOf('[P]');
+        // let's start by getting the current location of the player
+        var current_location = grid.indexOf('<i class=\"fas fa-child fa-fw\" style=\"color:red\"></i>');
         // the line below is for debugging
         console.log(current_location);
-        // now the destination. This ASSUMES A 24 LENGTH array
+        // now the destination. This ASSUMES A 34 LENGTH array
         var destination = current_location - 1;
         // now lets replace the terrain that was in the old place.
         grid[destination+1] = destination_terrain;
         // now let's get the terrain the place they want to go. we need this so we can replace it when they move later on. 
         destination_terrain = grid[current_location - 1];
         // now let's move the player icon. 
-        grid[destination] = '[P]';
+        grid[destination] = '<i class=\"fas fa-child fa-fw\" style=\"color:red\"></i>';
+        document.getElementById("messages").innerHTML = current_location;
 
 
     } else if (direction === 'u'){
-        // let's start by getting ther current location of the player
-        var current_location = grid.indexOf('[P]');
+        // let's start by getting the current location of the player
+        var current_location = grid.indexOf('<i class=\"fas fa-child fa-fw\" style=\"color:red\"></i>');
         // the line below is for debugging
         console.log(current_location);
-        // now the destination. This ASSUMES A 24 LENGTH array
-        var destination = current_location - 24;
+        // now the destination. This ASSUMES A 34 LENGTH array
+        var destination = current_location - 34;
         // now lets replace the terrain that was in the old place.
-        grid[destination+24] = destination_terrain;
+        grid[destination + 34] = destination_terrain;
         // now let's get the terrain the place they want to go. we need this so we can replace it when they move later on. 
-        destination_terrain = grid[current_location - 24];
+        destination_terrain = grid[current_location - 34];
         // now let's move the player icon. 
-        grid[destination] = '[P]';
+        grid[destination] = '<i class=\"fas fa-child fa-fw\" style=\"color:red\"></i>';
+        document.getElementById("messages").innerHTML = current_location;
 
     } else if (direction === 'd'){
-        // let's start by getting ther current location of the player
-        var current_location = grid.indexOf('[P]');
+        // let's start by getting the current location of the player
+        var current_location = grid.indexOf('<i class=\"fas fa-child fa-fw\" style=\"color:red\"></i>');
         // the line below is for debugging
         console.log(current_location);
-        // now the destination. This ASSUMES A 24 LENGTH array
-        var destination = current_location + 24;
+        // now the destination. This ASSUMES A 34 LENGTH array
+        var destination = current_location + 34;
         // now lets replace the terrain that was in the old place.
-        grid[destination-24] = destination_terrain;
+        grid[destination - 34] = destination_terrain;
         // now let's get the terrain the place they want to go. we need this so we can replace it when they move later on. 
-        destination_terrain = grid[current_location + 24];
+        destination_terrain = grid[current_location + 34];
         // now let's move the player icon. 
-        grid[destination] = '[P]';
+        grid[destination] = '<i class=\"fas fa-child fa-fw\" style=\"color:red\"></i>';
+        document.getElementById("messages").innerHTML = current_location;
 
     return
   }
@@ -170,8 +170,6 @@ document.addEventListener('keyup', function (event) {
   if (key === 'Escape' || key === 'Esc' || key === 27) {
     document.getElementById("messages").innerHTML = key;
     } else if (key === 'ArrowRight' || key === 39) {
-        // the line below is a debug message which shows the keypress
-        document.getElementById("messages").innerHTML = key;
         // then we call the move function and pass 'r' for right.
         move('r')
         // finally, we update the map after the move has been completed (but this might no belong here).
@@ -179,8 +177,6 @@ document.addEventListener('keyup', function (event) {
         
         
     } else if (key === 'ArrowLeft' || key === 39) {
-        // the line below is a debug message which shows the keypress
-        document.getElementById("messages").innerHTML = key;
         // then we call the move function and pass 'r' for right.
         move('l')
         // finally, we update the map after the move has been completed (but this might no belong here).
@@ -189,8 +185,6 @@ document.addEventListener('keyup', function (event) {
 
 
     } else if (key === 'ArrowUp' || key === 39) {
-        // the line below is a debug message which shows the keypress
-        document.getElementById("messages").innerHTML = key;
         // then we call the move function and pass 'r' for right.
         move('u')
         // finally, we update the map after the move has been completed (but this might no belong here).
@@ -202,8 +196,6 @@ document.addEventListener('keyup', function (event) {
 
 
     } else if (key === 'ArrowDown' || key === 39) {
-        // the line below is a debug message which shows the keypress
-        document.getElementById("messages").innerHTML = key;
         // then we call the move function and pass 'r' for right.
         move('d')
         // finally, we update the map after the move has been completed (but this might no belong here).
