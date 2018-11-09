654e4ffe02f9ade6dd81a5bddd7672930bf77549
Bill MacKenty <bmackenty@users.noreply.github.com>
2018-11-09 11:58:44 +0100

    got the keypress stuff and fixed terrain
    Thank you to Ellie, JP and Jack for their help :-)

--- script.js
+++ script.js
@@ -41,27 +41,42 @@ var grid = [];
 var terrain;
 
 var counter = 1;
-while ( counter <= 1000 ) {
+while ( counter <= 695 ) {
   // the line below creates some random terrain. totally random, TODO: this needs to be smart.
-  terrain = Math.floor(Math.random() * 20)+1;
+  terrain = Math.floor(Math.random() * 5)+1;
   grid.push(terrain);
   counter += 1;
 }
 
+// make player starting location
+grid.splice(100, 0, 6);
+
 function draw_map(grid) {
 
+var row_length = 10;
+var counter = 0;
 var arrayLength = grid.length;
 for (var i = 0; i < arrayLength; i++) {
+
+  counter = counter +1;
+  if(counter == 24) {
+    grid[i] = "\n";
+    counter = 0;
+  }
+
   if (grid[i] === 1 || grid[i] === 2 || grid[i] === 3){
     // this is plain, open terrain
     grid[i] = "...";
   } else if (grid[i] === 4) {
     // this is a tree
-    grid[i] = "*";
+    grid[i] = ".*.";
   } else if (grid[i] === 5) {
     // this is a mountain or hill
-    grid[i] = "^";
-  }
+    grid[i] = ".^.";
+  } else if (grid[i] === 6) {
+  // this is a mountain or hill
+  grid[i] = "[P]";
+}
 
 }
 grid = grid.join('');
@@ -69,6 +84,27 @@ return grid;
 }
 document.getElementById("main_map").innerHTML = draw_map(grid);
 
-// for (key in player) {
-//   console.log(key, ' : ', player[key])
-// }
+document.getElementById("footer").innerHTML = "Health: " + player.health + " | Magic: " + player.magic;
+
+function move() {
+
+  alert('hello');
+
+}
+
+
+// the code below is used from https://medium.com/@uistephen/keyboardevent-key-for-cross-browser-key-press-check-61dbad0a067a
+
+document.addEventListener('keyup', function (event) {
+  if (event.defaultPrevented) {
+      return;
+  }
+
+  var key = event.key || event.keyCode;
+
+  if (key === 'Escape' || key === 'Esc' || key === 27) {
+    document.getElementById("messages").innerHTML = draw_map(grid);
+
+  }
+});
+
